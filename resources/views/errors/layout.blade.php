b9b8cef81b04537f8ec1bee2cf7722a67eb13e8b
Lenovo <asadbek.polatov.02@mail.ru>
2022-05-06 16:11:57 +0500

    Error code adding

---
+++ resources/views/errors/layout.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>W3soft</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="{{ asset('assets/img/favicon.ico') }}" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('assets/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+@yield('message')
+
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('assets/lib/wow/wow.min.js') }}"></script>
+<script src="{{ asset('assets/lib/easing/easing.min.js') }}"></script>
+<script src="{{ asset('assets/lib/waypoints/waypoints.min.js') }}"></script>
+<script src="{{ asset('assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('assets/lib/isotope/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('assets/lib/lightbox/js/lightbox.min.js') }}"></script>
+
+<!-- Template Javascript -->
+<script src="{{ asset('assets/js/main.js') }}"></script>
+</body>
+
+</html>
+
+{{--<!DOCTYPE html>--}}
+{{--<html lang="en">--}}
+{{--    <head>--}}
+{{--        <meta charset="utf-8">--}}
+{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
+
+{{--        <title>@yield('title')</title>--}}
+
+{{--        <!-- Styles -->--}}
+{{--        <style>--}}
+{{--            html, body {--}}
+{{--                background-color: #fff;--}}
+{{--                color: #636b6f;--}}
+{{--                font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";--}}
+{{--                font-weight: 100;--}}
+{{--                height: 100vh;--}}
+{{--                margin: 0;--}}
+{{--            }--}}
+
+{{--            .full-height {--}}
+{{--                height: 100vh;--}}
+{{--            }--}}
+
+{{--            .flex-center {--}}
+{{--                align-items: center;--}}
+{{--                display: flex;--}}
+{{--                justify-content: center;--}}
+{{--            }--}}
+
+{{--            .position-ref {--}}
+{{--                position: relative;--}}
+{{--            }--}}
+
+{{--            .content {--}}
+{{--                text-align: center;--}}
+{{--            }--}}
+
+{{--            .title {--}}
+{{--                font-size: 36px;--}}
+{{--                padding: 20px;--}}
+{{--            }--}}
+{{--        </style>--}}
+{{--    </head>--}}
+{{--    <body>--}}
+{{--        <div class="flex-center position-ref full-height">--}}
+{{--            <div class="content">--}}
+{{--                <div class="title">--}}
+{{--                    @yield('message')--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </body>--}}
+{{--</html>--}}
