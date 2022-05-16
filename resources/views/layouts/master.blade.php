8d39a5f014b54528d30290d75e55981f760c6c8c
Lenovo <asadbek.polatov.02@mail.ru>
2022-05-16 15:43:04 +0500

    .

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -126,7 +126,10 @@
         <div class="container px-lg-5">
             <div class="copyright">
                 <div class="row">
-
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
+                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+                    </div>
                 </div>
             </div>
         </div>
